f1cfc78df4e00c316c73e0f4e4818f92f3cb3430
catornot <41955154+catornot@users.noreply.github.com>
2025-01-17 21:14:26 -0500

    make this into a derivation 2

--- pkgs/papa/default.nix
+++ pkgs/papa/default.nix
@@ -3,18 +3,22 @@
   openssl,
   stdenv,
   lib,
+  autoPatchelfHook,
 }:
 let
 in
 stdenv.mkDerivation rec {
   pname = "papa";
   version = "4.0.0";
-  
+
   src = builtins.fetchurl {
-   url = "https://github.com/AnActualEmerald/papa/releases/download/v4.0.0/papa";
-   sha256 = "sha256:12gqm4mfvac0cybkdhx9c7617anx377himham06asd4xv8i99ci5";
+    url = "https://github.com/AnActualEmerald/papa/releases/download/v4.0.0/papa";
+    sha256 = "sha256:12gqm4mfvac0cybkdhx9c7617anx377himham06asd4xv8i99ci5";
   };
 
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
   buildInputs = [
     pkg-config
     openssl
@@ -22,9 +26,11 @@ stdenv.mkDerivation rec {
 
   sourceRoot = ".";
 
+  phases = [ "installPhase" ];
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
-    install -m755 -D papa $out/bin/papa
+    install -m755 -D ${src} $out/bin/papa
     runHook postInstall
   '';
 
@@ -37,4 +43,3 @@ stdenv.mkDerivation rec {
     maintainers = [ ];
   };
 }
-
